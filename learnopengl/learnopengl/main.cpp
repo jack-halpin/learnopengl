e6ba57feca513d63ad1c5b7a1252d41b470db69e
Jack Halpin <jhalpin92@gmail.com>
2019-08-04 17:16:12 +0100

    Added CMakeLists.txt @

---
+++ learnopengl/learnopengl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learnopengl
+//
+//  Created by Jack Halpin on 29/07/2019.
+//  Copyright © 2019 Jack Halpin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
