7efe1400d063777edb2262b171626a0804cb7e23
Jack Halpin <jhalpin92@gmail.com>
2021-02-12 14:21:12 +0000

    Updating cmake@

--- include/shader.h
+++ include/shader.h
@@ -4,6 +4,8 @@
 #define SHADER_H
 
 #include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include <string>
 #include <fstream>
@@ -116,6 +118,16 @@ public:
 	void setFloat(const std::string& name, float value)
 	{
 		glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+	}
+
+	void setMat4(const std::string& name, glm::mat4 value)
+	{
+		glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
+	}
+
+	void setVec3(const std::string& name, glm::vec3 value)
+	{
+		glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, glm::value_ptr(value));
 	}
 };
 
